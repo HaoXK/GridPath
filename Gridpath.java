92f3dec8047c9230f209f6d02ca29c3c60d23f5e
HaoXK <haoxuank@nycstudents.net>
2024-11-12 12:23:55 -0500

    Create Gridpath.java

---
+++ Gridpath.java
@@ -0,0 +1,14 @@
+public class GridPath {
+
+    private int[][] grid;
+
+    public Location getNextLoc(int row, int col) 
+    {
+
+    }
+       
+    public int sumPath(int row, int col) 
+    {
+
+    }
+}
