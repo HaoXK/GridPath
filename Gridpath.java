ff6a84411926bde45778b73662be34d7bcfaf7cb
HaoXK <haoxuank@nycstudents.net>
2024-11-18 12:33:50 -0500

    Update Gridpath.java

--- Gridpath.java
+++ Gridpath.java
@@ -1,12 +1,14 @@
 public class GridPath {
 
     private int[][] grid;
-
+    public GridPath(int[][] values) {
+        grid = values;
+    }
     public Location getNextLoc(int row, int col) 
     {
 
     }
-       
+    public String toString
     public int sumPath(int row, int col) 
     {
 
