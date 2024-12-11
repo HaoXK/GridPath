490418bb9de3f3a79ba65ea52cedc00b77866d5d
HaoXK <haoxuank@nycstudents.net>
2024-12-11 12:27:15 -0500

    Update Gridpath.java

--- Gridpath.java
+++ Gridpath.java
@@ -4,13 +4,34 @@ public class GridPath {
     public GridPath(int[][] values) {
         grid = values;
     }
-    public Location getNextLoc(int row, int col) 
-    {
-
+    public Location getNextLoc(int row, int col) {
+        int below = Integer.MAX_VALUE;
+        if (row < grid.length - 1) below = grid[row + 1][col];
+        int right = Integer.MAX_VALUE;
+        if (col < grid[0].length - 1) right = grid[row][col + 1];
+        if (below < right) return new Location(row + 1, col);
+        else return new Location(row, col + 1);
     }
-    public String toString
+    public String toString() 
+    {
+        String s = "";
+        for (int[] row : grid) {
+            for (int col : row) {
+                s += col + " ";
+            }
+            s += "/n";
+        }
+        return s;
+    }
     public int sumPath(int row, int col) 
     {
-
+    int sum = grid[row][col];
+    while(row < grid.length - 1 || col < grid[0].length - 1) {
+        Location loc = getNextLoc(row, col);
+        row = loc.getRow();
+        col = loc.getCol();
+        sum += grid[row][col];
+    }
+    return sum;
     }
 }
